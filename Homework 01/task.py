839b42bd6083ffc725c55ddfbc8e60cc3e76b3e3
Maxim Vasilyev <mayxis@inbox.ru>
2023-10-10 17:08:44 +0300

    new: add hw01

---
+++ Homework 01/task.py
@@ -0,0 +1,207 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import matplotlib
+import copy
+import pandas
+from typing import NoReturn, Tuple, List
+
+
+# Task 1
+
+def read_cancer_dataset(path_to_csv: str) -> Tuple[np.array, np.array]:
+    """
+     
+    Parameters
+    ----------
+    path_to_csv : str
+        Путь к cancer датасету.
+
+    Returns
+    -------
+    X : np.array
+        Матрица признаков опухолей.
+    y : np.array
+        Вектор бинарных меток, 1 соответствует доброкачественной опухоли (M), 
+        0 --- злокачественной (B).
+
+    
+    """
+    raise NotImplementedError()
+
+def read_spam_dataset(path_to_csv: str) -> Tuple[np.array, np.array]:
+    """
+     
+    Parameters
+    ----------
+    path_to_csv : str
+        Путь к spam датасету.
+
+    Returns
+    -------
+    X : np.array
+        Матрица признаков сообщений.
+    y : np.array
+        Вектор бинарных меток, 
+        1 если сообщение содержит спам, 0 если не содержит.
+    
+    """
+    raise NotImplementedError()
+    
+# Task 2
+
+def train_test_split(X: np.array, y: np.array, ratio: float) -> Tuple[np.array, np.array, np.array, np.array]:
+    """
+
+    Parameters
+    ----------
+    X : np.array
+        Матрица признаков.
+    y : np.array
+        Вектор меток.
+    ratio : float
+        Коэффициент разделения.
+
+    Returns
+    -------
+    X_train : np.array
+        Матрица признаков для train выборки.
+    y_train : np.array
+        Вектор меток для train выборки.
+    X_test : np.array
+        Матрица признаков для test выборки.
+    y_test : np.array
+        Вектор меток для test выборки.
+
+    """
+    raise NotImplementedError()
+    
+# Task 3
+
+def get_precision_recall_accuracy(y_pred: np.array, y_true: np.array) -> Tuple[np.array, np.array, float]:
+    """
+
+    Parameters
+    ----------
+    y_pred : np.array
+        Вектор классов, предсказанных моделью.
+    y_true : np.array
+        Вектор истинных классов.
+
+    Returns
+    -------
+    precision : np.array
+        Вектор с precision для каждого класса.
+    recall : np.array
+        Вектор с recall для каждого класса.
+    accuracy : float
+        Значение метрики accuracy (одно для всех классов).
+
+    """
+    raise NotImplementedError()
+    
+# Task 4
+
+class KDTree:
+    def __init__(self, X: np.array, leaf_size: int = 40):
+        """
+
+        Parameters
+        ----------
+        X : np.array
+            Набор точек, по которому строится дерево.
+        leaf_size : int
+            Минимальный размер листа
+            (то есть, пока возможно, пространство разбивается на области, 
+            в которых не меньше leaf_size точек).
+
+        Returns
+        -------
+
+        """        
+        raise NotImplementedError()
+    
+    def query(self, X: np.array, k: int = 1) -> List[List]:
+        """
+
+        Parameters
+        ----------
+        X : np.array
+            Набор точек, для которых нужно найти ближайших соседей.
+        k : int
+            Число ближайших соседей.
+
+        Returns
+        -------
+        list[list]
+            Список списков (длина каждого списка k): 
+            индексы k ближайших соседей для всех точек из X.
+
+        """
+        
+        raise NotImplementedError()
+        
+# Task 5
+
+class KNearest:
+    def __init__(self, n_neighbors: int = 5, leaf_size: int = 30):
+        """
+
+        Parameters
+        ----------
+        n_neighbors : int
+            Число соседей, по которым предсказывается класс.
+        leaf_size : int
+            Минимальный размер листа в KD-дереве.
+
+        """        
+        raise NotImplementedError()    
+    
+    def fit(self, X: np.array, y: np.array) -> NoReturn:
+        """
+
+        Parameters
+        ----------
+        X : np.array
+            Набор точек, по которым строится классификатор.
+        y : np.array
+            Метки точек, по которым строится классификатор.
+
+        """        
+        raise NotImplementedError()
+        
+    def predict_proba(self, X: np.array) -> List[np.array]:
+        """
+
+        Parameters
+        ----------
+        X : np.array
+            Набор точек, для которых нужно определить класс.
+        
+        Returns
+        -------
+        list[np.array]
+            Список np.array (длина каждого np.array равна числу классов):
+            вероятности классов для каждой точки X.
+            
+
+        """
+    
+        raise NotImplementedError()
+        
+    def predict(self, X: np.array) -> np.array:
+        """
+
+        Parameters
+        ----------
+        X : np.array
+            Набор точек, для которых нужно определить класс.
+        
+        Returns
+        -------
+        np.array
+            Вектор предсказанных классов.
+            
+
+        """
+        return np.argmax(self.predict_proba(X), axis=1)
