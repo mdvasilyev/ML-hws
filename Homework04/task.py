580b162362c3f740e2b3ed7bb6e2310c900ae3e1
Maxim Vasilyev <mayxis@inbox.ru>
2023-11-05 12:09:41 +0300

    new: add hw04

---
+++ Homework04/task.py
@@ -0,0 +1,148 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+import copy
+from typing import NoReturn
+
+
+# Task 1
+
+class Perceptron:
+    def __init__(self, iterations: int = 100):
+        """
+        Parameters
+        ----------
+        iterations : int
+        Количество итераций обучения перцептрона.
+
+        Attributes
+        ----------
+        w : np.ndarray
+        Веса перцептрона размерности X.shape[1] + 1 (X --- данные для обучения), 
+        w[0] должен соответстовать константе, 
+        w[1:] - коэффициентам компонент элемента X.
+
+        Notes
+        -----
+        Вы можете добавлять свои поля в класс.
+        
+        """
+
+        self.w = None
+    
+    def fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
+        """
+        Обучает простой перцептрон. 
+        Для этого сначала инициализирует веса перцептрона,
+        а затем обновляет их в течении iterations итераций.
+        
+        Parameters
+        ----------
+        X : np.ndarray
+            Набор данных, на котором обучается перцептрон.
+        y: np.ndarray
+            Набор меток классов для данных.
+        
+        """
+        pass
+            
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Предсказывает метки классов.
+        
+        Parameters
+        ----------
+        X : np.ndarray
+            Набор данных, для которого необходимо вернуть метки классов.
+        
+        Return
+        ------
+        labels : np.ndarray
+            Вектор индексов классов 
+            (по одной метке для каждого элемента из X).
+        
+        """
+        pass
+    
+# Task 2
+
+class PerceptronBest:
+
+    def __init__(self, iterations: int = 100):
+        """
+        Parameters
+        ----------
+        iterations : int
+        Количество итераций обучения перцептрона.
+
+        Attributes
+        ----------
+        w : np.ndarray
+        Веса перцептрона размерности X.shape[1] + 1 (X --- данные для обучения), 
+        w[0] должен соответстовать константе, 
+        w[1:] - коэффициентам компонент элемента X.
+
+        Notes
+        -----
+        Вы можете добавлять свои поля в класс.
+        
+        """
+
+        self.w = None
+    
+    def fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
+        """
+        Обучает перцептрон.
+
+        Для этого сначала инициализирует веса перцептрона, 
+        а затем обновляет их в течении iterations итераций.
+
+        При этом в конце обучения оставляет веса, 
+        при которых значение accuracy было наибольшим.
+        
+        Parameters
+        ----------
+        X : np.ndarray
+            Набор данных, на котором обучается перцептрон.
+        y: np.ndarray
+            Набор меток классов для данных.
+        
+        """
+        pass
+            
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Предсказывает метки классов.
+        
+        Parameters
+        ----------
+        X : np.ndarray
+            Набор данных, для которого необходимо вернуть метки классов.
+        
+        Return
+        ------
+        labels : np.ndarray
+            Вектор индексов классов 
+            (по одной метке для каждого элемента из X).
+        
+        """
+        pass
+    
+# Task 3
+
+def transform_images(images: np.ndarray) -> np.ndarray:
+    """
+    Переводит каждое изображение в вектор из двух элементов.
+        
+    Parameters
+    ----------
+    images : np.ndarray
+        Трехмерная матрица с черное-белыми изображениями.
+        Её размерность: (n_images, image_height, image_width).
+
+    Return
+    ------
+    np.ndarray
+        Двумерная матрица с преобразованными изображениями.
+        Её размерность: (n_images, 2).
+    """
+    return np.zeros((images.shape[0], 2))
