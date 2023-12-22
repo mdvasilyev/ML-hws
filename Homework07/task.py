630850b8b7e5575b944bab9b28ef68c8ad15295c
Maxim Vasilyev <mayxis@inbox.ru>
2023-12-22 23:54:55 +0300

    sln: add current version of sln

--- Homework07/task.py
+++ Homework07/task.py
@@ -75,11 +75,13 @@ class DecisionTreeNode:
     """
     def __init__(self, split_dim: int, split_value: float, 
                  left: Union['DecisionTreeNode', DecisionTreeLeaf], 
-                 right: Union['DecisionTreeNode', DecisionTreeLeaf]):
+                 right: Union['DecisionTreeNode', DecisionTreeLeaf],
+                 info_gain: float = {}):
         self.split_dim = split_dim
         self.split_value = split_value
         self.left = left
         self.right = right
+        self.info_gain = info_gain
         
 # Task 3
 
@@ -109,7 +111,43 @@ class DecisionTreeClassifier:
 
         """
         self.root = None
+        self.criterion = criterion
+        self.max_depth = max_depth
+        self.min_samples_leaf = min_samples_leaf
     
+    def build(self, dataset: np.ndarray, cur_depth: int):
+        X, y = dataset[:, :-1], dataset[:, -1]
+        samples, features = np.shape(X)
+        if self.max_depth is None: self.max_depth = 10e6
+        if samples >= self.min_samples_leaf and cur_depth <= self.max_depth:
+            split = self.get_split(dataset, features)
+            if split["info_gain"] > 0:
+                left = self.build(split["left"], cur_depth + 1)
+                right = self.build(split["right"], cur_depth + 1)
+                return DecisionTreeNode(split["split_dim"], split["split_value"], left, right, split["info_gain"])
+        return DecisionTreeLeaf(y)
+    
+    def get_split(self, dataset: np.ndarray, features: int):
+        split = {}
+        max_info_gain = -10e6
+        for split_dim in range(features):
+            feature_values = dataset[:, split_dim]
+            possible_thresholds = np.unique(feature_values)
+            for split_value in possible_thresholds:
+                left_dataset = np.array([row for row in dataset if row[split_dim] < split_value])
+                right_dataset = np.array([row for row in dataset if row[split_dim] >= split_value])
+                if len(left_dataset) > 0 and len(right_dataset) > 0:
+                    y, left_y, right_y = dataset[:, -1], left_dataset[:, -1], right_dataset[:, -1]
+                    cur_info_gain = gain(left_y, right_y, globals()[self.criterion])
+                    if cur_info_gain > max_info_gain:
+                        max_info_gain = cur_info_gain
+                        split["split_dim"] = split_dim
+                        split["split_value"] = split_value
+                        split["left"] = left_dataset
+                        split["right"] = right_dataset
+                        split["info_gain"] = cur_info_gain
+        return split
+
     def fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
         """
         Строит дерево решений по обучающей выборке.
@@ -121,7 +159,8 @@ class DecisionTreeClassifier:
         y : np.ndarray
             Вектор меток классов.
         """
-        pass
+        dataset = np.hstack((X, np.transpose([y])))
+        self.root = self.build(dataset, cur_depth=0)
     
     def predict_proba(self, X: np.ndarray) ->  List[Dict[Any, float]]:
         """
@@ -138,8 +177,19 @@ class DecisionTreeClassifier:
             Для каждого элемента из X возвращает словарь 
             {метка класса -> вероятность класса}.
         """
-        
-        pass
+        proba = {}
+        for x in X:
+            proba[x] = self.traverse_tree(x, self.root)
+        return proba
+    
+    def traverse_tree(self, x, tree: Union[DecisionTreeNode, DecisionTreeLeaf]):
+        if tree.__class__.__name__ == "DecisionTreeLeaf":
+            return tree.y
+        value = x[tree.split_dim]
+        if value < tree.split_value:
+            return self.traverse_tree(x, tree.left)
+        else:
+            return self.traverse_tree(x, tree.right)
     
     def predict(self, X : np.ndarray) -> list:
         """
